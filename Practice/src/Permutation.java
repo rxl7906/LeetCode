73a3d7758f35c7973ba0f25561e3b249ee33d8bb
rxl79 <rxl79@DESKTOP-6AMJGHQ.wireless.rit.edu>
2016-10-07 14:14:54 -0400

    c

--- Practice/src/Permutation.java
+++ Practice/src/Permutation.java
@@ -6,7 +6,8 @@ import java.util.Set;
 
 public class Permutation {
 	public static Set<String> ar = new LinkedHashSet<String>();
-	
+	public static String origStr;
+	public static int count = 0;
 	public static void permutation(String str) { 
 	    permutation("", str); 
 	}
@@ -15,44 +16,21 @@ public class Permutation {
 	    int n = str.length();
 	    if (n == 0) {
 	    	ar.add(prefix);
-	    	//System.out.println(prefix);
+	    } else {
+	        for (int i = 0; i < n; i++){
+	        	permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i+1, n));
+	        }
 	    }
-	    else {
-	        for (int i = 0; i < n; i++)
-	            permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i+1, n));
-	    }
-	}
-	public static Set<String> crunchifyPermutation(String str) {
-		Set<String> crunchifyResult = new HashSet<String>();
-		if (str == null) {
-			return null;
-		} else if (str.length() == 0) {
-			crunchifyResult.add("");
-			return crunchifyResult;
-		}
- 
-		char firstChar = str.charAt(0);
-		String rem = str.substring(1);
-		Set<String> words = crunchifyPermutation(rem);
-		for (String newString : words) {
-			for (int i = 0; i <= newString.length(); i++) {
-				crunchifyResult.add(crunchifyCharAdd(newString, firstChar, i));
-			}
-		}
-		return crunchifyResult;
-	}
- 
-	public static String crunchifyCharAdd(String str, char c, int j) {
-		String first = str.substring(0, j);
-		String last = str.substring(j);
-		return first + c + last;
 	}
 	
 	public static int findRank(String str){
 		char[] arr = str.toCharArray();
 		Arrays.sort(arr);
 		//System.out.println(String.copyValueOf(arr));
+		origStr = str;
 		permutation(String.copyValueOf(arr));
+		System.out.println(count);
+		
 		int c = 0;
 		for(String s : ar){
 			if(s.equals(str)){
@@ -66,5 +44,14 @@ public class Permutation {
 	public static void main(String args[]){
 		String str = "caabbc";
 		System.out.println(findRank(str));
+		int c = 0;
+		for(String s : ar){
+			if(s.equals(str)){
+				break;
+			}
+			System.out.println(s);
+			c++;
+		}
+		System.out.println(c);
 	}
 }
